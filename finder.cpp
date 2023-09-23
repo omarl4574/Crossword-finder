b771c23d3fcda6dfc250a07bdaac253b9d2ff6ee
Omar Lopez <omarl4574@csu.fullerton.edu>
2023-09-23 15:47:19 -0700

    changed the style to fit with the google c++ coding style

--- finder.cpp
+++ finder.cpp
@@ -8,75 +8,77 @@
 
 using namespace std;
 
-// The function that set up the board as a 2D array to then sort through later and find all the words
-vector<vector<string>> SetUpBoard(int amount_rows, int amount_cols, string letters){
-    vector<vector<string>> board(amount_rows, vector<string>(amount_cols));
+/* The function that set up the board as a 2D array to then sort through later
+and find all the words */
+vector<vector<string>> SetUpBoard(int amount_rows, int amount_cols,
+                                  string letters) {
+  vector<vector<string>> board(amount_rows, vector<string>(amount_cols));
 
-    int letter_index = 0;
-    for(int rows = 0; rows < amount_rows; rows++){
-        for(int cols = 0; cols < amount_cols; cols++){
-            board[rows][cols] = letters[letter_index];
-            letter_index++;
-        }
+  int letter_index = 0;
+  for (int rows = 0; rows < amount_rows; rows++) {
+    for (int cols = 0; cols < amount_cols; cols++) {
+      board[rows][cols] = letters[letter_index];
+      letter_index++;
     }
-    return board;
+  }
+  return board;
 }
 
 // Prints the cross word puzzle board
-void PrintBoard(vector<vector<string>> board){
-    for(int rows = 0; rows < board.size(); rows++){
-        for(int cols = 0; cols < board[rows].size(); cols++){
-            cout << board[rows][cols] << " ";
-        }
-        cout << "\n";
+void PrintBoard(vector<vector<string>> board) {
+  for (int rows = 0; rows < board.size(); rows++) {
+    for (int cols = 0; cols < board[rows].size(); cols++) {
+      cout << board[rows][cols] << " ";
     }
+    cout << "\n";
+  }
 }
 
-/* asks the user for all the words they need to find in the cross word puzzle they entered 
-and stores every word they entered in a vector called words */
+/* asks the user for all the words they need to find in the cross word puzzle
+they entered and stores every word they entered in a vector called words */
 
-vector<string> GetAllWords(){
-    vector<string> words;
+vector<string> GetAllWords() {
+  vector<string> words;
 
-    while(true){
-        cout << "Enter words (type 'quit' exit): ";
-        string word;
-        cin >> word;
+  while (true) {
+    cout << "Enter words (type 'quit' exit): ";
+    string word;
+    cin >> word;
 
-        if(word == "quit"){
-            break;
-        }
-
-        words.push_back(word);
+    if (word == "quit") {
+      break;
     }
 
-    return words;
+    words.push_back(word);
+  }
+
+  return words;
 }
 
 // prints all the words the user has entered from the words vector
-void PrintWords(vector<string> words){
-    for(int i = 0; i < words.size(); i++){
-        cout << words[i] << ", ";
-    }
-    cout << "\n";
+void PrintWords(vector<string> words) {
+  for (int i = 0; i < words.size(); i++) {
+    cout << words[i] << ", ";
+  }
+  cout << "\n";
 }
 
-int main(){
-    int num_rows{0};
-    int num_cols{0};
+int main() {
+  int num_rows{0};
+  int num_cols{0};
 
-    cout << "Enter the amount of rows your board has: ";
-    cin >> num_rows;
+  cout << "Enter the amount of rows your board has: ";
+  cin >> num_rows;
 
-    cout << "Enter the amount of columns your board has: ";
-    cin >> num_cols;
+  cout << "Enter the amount of columns your board has: ";
+  cin >> num_cols;
 
-    string letters{"qwerty"};
-    cout << "Enter the board all horizontally: ";
-    cin >> letters;
+  string letters{"qwerty"};
+  cout << "Enter the board all horizontally: ";
+  cin >> letters;
 
-    vector<vector<string>> board{SetUpBoard(num_rows, num_cols, letters)};
-    vector<string> words{GetAllWords()};
-    
-    return 0;
+  vector<vector<string>> board{SetUpBoard(num_rows, num_cols, letters)};
+  vector<string> words{GetAllWords()};
+
+  return 0;
 }
